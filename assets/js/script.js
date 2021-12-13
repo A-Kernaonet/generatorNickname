28a8b57f323e74e6485e76d74f4f286c19b7e82b
Elena TASQUIER <elena-tasquier@hotmail.fr>
2021-12-13 11:55:39 +0100

    creation tableaux

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function($) {
+
+    var mois = ['Chameau', 'Cafard', 'Bouc', 'Pigeon', 'Blaireau', 'Chacal', 'Dindon', 'Poulet', 'Crapaud', 'Putois', 'Blobfish', 'Bousier'];
+
+    var jour = ['debile', 'moisi', 'peteur', 'de l\'espace', 'malefique', 'rampant', 'furieux', 'pourri', 'malin', 'boiteux', 'mortel', 'sexuel', 'bigleux', 'cretin', 'genial', 'sanglant', 'magique', 'supreme', 'charmant', 'bouseux', 'vengeur', 'fetide', 'infernal', 'crado', 'de carnaval', 'puant', 'hurleur', 'somptueux', 'mou', 'mignon'];
+
+});
